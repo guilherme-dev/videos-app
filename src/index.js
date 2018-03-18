9e8abf0193d2aa7f028717de8a8e7559996ac2ab
Guilherme Santos <ggs12@inf.ufpr.br>
2018-03-18 00:29:25 -0300

    first commit, initial code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+
+
+// const API_KEY = 'AIzaSyCXcEG_Wa4FRsj2nXBRq3Aef9REqO0unt8';
+
+class App extends Component {
+
+	render() {
+		return (
+			<SearchBar />
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
